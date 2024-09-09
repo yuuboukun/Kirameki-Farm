7e080e1413ebbd87a88c210375806ecc3203ee73
yuuboukun <pikahikari87@gmail.com>
2024-09-09 12:26:33 +0900

    240904_yuuzou.yamadaのブランチ011

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,30 @@
-// ハンバーガーメニュー
+/**
+ * ハンバーガーメニュー
+ */
 var nav = $('.header__nav');
 
 // ボタンをクリックしたら
 $('.header__hamburger-btn').on('click', function () {
-  // ナビゲーションの表示・非表示を繰り返す
-  nav.toggleClass('header__nav--active');
+    // ナビゲーションの表示・非表示を繰り返す
+    nav.toggleClass('header__nav--active');
 });
 
 // 画面幅のサイズが変わったら
 $(window).on('resize', function () {
-  // ハンバーガーメニューを閉じる
-  nav.removeClass('header__nav--active');
+    // ハンバーガーメニューを閉じる
+    nav.removeClass('header__nav--active');
 });
+
+/**
+ * アコーディオン
+ */
+$('.faq__question').on('click', function () {
+        // アンサーを選択
+        var answer = $(this).next();
+
+        // アンサーを表示非表示
+        answer.slideToggle();
+
+        // アイコンの入れ替え
+        $(this).find('.bi').toggleClass('replace');
+})
